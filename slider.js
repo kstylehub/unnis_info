7832fd09d6b6dd73b1c9e6d80025a5c2d8b99bcc
yesoljeong <yesol0523@gmail.com>
2020-12-17 15:00:00 +0900

    slider 스타일 변경 / contact section 추가

--- slider.js
+++ slider.js
@@ -3,8 +3,6 @@ const sliderLis = document.querySelectorAll(".slider_img_text li")
 const sliderImgs = document.querySelectorAll(".slider_img_text img")
 const sliderText = document.querySelectorAll(".slider_img_text li > div")
 
-console.log(sliderText)
-
 // tads 첫번째껄 누르면 display none에서 block으로 바뀜
 
 for (let i = 0; i < tabs.length; i++) {
@@ -18,11 +16,21 @@ function clickWork(e) {
     // 나머지 li들은 display none으로 바꾼다
 
     for (let i = 0; i < tabs.length; i++) {
-        if (e.target === tabs[i])
+        if (e.target === tabs[i]) {
             sliderLis[i].classList.add('on')
-        else {
+        } else {
             sliderLis[i].classList.remove('on')
         }
     }
-
 }
+
+// 자동 슬라이딩
+setInterval(function () {
+
+    // 만약 class list에 on이 있으면 다음칭구한테 on을 줘라
+    for (let i = 0; i < sliderLis.length; i++) {
+        if (sliderLis[i].classList.contains('on')) {
+            sliderLis[i].nextElementSibling.classList.add('on')
+        } 
+    }
+}, 10000);
