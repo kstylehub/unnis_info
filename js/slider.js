f9d20bf8ca0cc727c105233bf5f5b8e107fb097a
yesoljeong <yesol0523@gmail.com>
2020-12-23 17:18:29 +0900

    리뷰섹션 슬릭슬라이더 추가

--- js/slider.js
+++ js/slider.js
@@ -74,9 +74,36 @@ setInterval(changeImg, 5000)
 
 // 슬릭 슬라이더 리뷰 섹션
 
-$(document).ready(function(){
+$(document).ready(function () {
+
+    //alert('123');
+
     $('.review_slider_wrap').slick({
-      setting-name: setting-value
+        autoplay: true,
+        dots: true,
+        speed: 300 /* 이미지가 슬라이딩시 걸리는 시간 */ ,
+        infinite: true,
+        autoplaySpeed: 3000 /* 이미지가 다른 이미지로 넘어 갈때의 텀 */ ,
+        arrows: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        fade: false,
+        responsive: [ // 반응형 웹 구현 옵션
+            {  
+                breakpoint: 960, //화면 사이즈 960px
+                settings: {
+                    //위에 옵션이 디폴트 , 여기에 추가하면 그걸로 변경
+                    slidesToShow:3 
+                } 
+            },
+            { 
+                breakpoint: 768, //화면 사이즈 768px
+                settings: {	
+                    //위에 옵션이 디폴트 , 여기에 추가하면 그걸로 변경
+                    slidesToShow:2 
+                } 
+            }
+        ]
     });
-  });
-      
+
+});
