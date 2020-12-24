e759e005b3dbe32597952ba9dcd3c88683a8089c
yesoljeong <yesol0523@gmail.com>
2020-12-24 17:55:14 +0900

    모바일 반응형 헤더 부분 수정 중

--- js/slider.js
+++ js/slider.js
@@ -84,7 +84,7 @@ $(document).ready(function () {
         speed: 300 /* 이미지가 슬라이딩시 걸리는 시간 */ ,
         infinite: true,
         autoplaySpeed: 3000 /* 이미지가 다른 이미지로 넘어 갈때의 텀 */ ,
-        arrows: true,
+        arrows: false,
         slidesToShow: 5,
         slidesToScroll: 1,
         fade: false,
