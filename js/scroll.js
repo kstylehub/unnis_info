6e312d9010f155135a1e6d385e01a37d01eca804
yesoljeong <yesol0523@gmail.com>
2020-12-29 17:34:06 +0900

    nav 햄버거 버튼 동작 구현 중

--- js/scroll.js
+++ js/scroll.js
@@ -10,13 +10,31 @@ function scrollWork() {
 
     if (html.scrollTop > 1700 && html.scrollTop < 1800) {
 
-        for(i = 0; i < calendarTexts.length; i++){
+        for (i = 0; i < calendarTexts.length; i++) {
             calendarTexts[i].classList.add('ani')
-        }   
-    }else if (html.scrollTop > 1000 && html.scrollTop < 1400) {
+        }
+    } else if (html.scrollTop > 1000 && html.scrollTop < 1400) {
 
         for (i = 0; i < cosmeticLis.length; i++) {
             cosmeticLis[i].classList.add('ani')
         }
-    } 
+    }
 }
+
+// navigation
+
+const hamburgerBtn = document.querySelector('.hamburger')
+const navUl = document.querySelector('.nav_wrap ul')
+console.log(navUl)
+
+hamburgerBtn.addEventListener('click', () => {
+
+    // 클릭하면 바뀌고 한번 더 클릭하면 원상태로
+    if (!hamburgerBtn.classList.contains('toggle')) {
+        hamburgerBtn.classList.add('toggle');
+        navUl.classList.add('on')
+    } else {
+        hamburgerBtn.classList.remove('toggle')
+        navUl.classList.remove('on')
+    }
+});
