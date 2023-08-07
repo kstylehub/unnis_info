98ea933bae1a810f15b63908454d87afdddc768f
ImChiKko <80472653+Danielkurniawan17@users.noreply.github.com>
2023-08-07 16:59:15 +0700

    Update detail event

--- unnis_info/src/Components/MobilePage/Components/ContentEvent/DetailEvent.jsx
+++ unnis_info/src/Components/MobilePage/Components/ContentEvent/DetailEvent.jsx
@@ -42,161 +42,6 @@ function DetailEvent() {
   const imageUrl =
     "https://wordpressunnis.s3.ap-southeast-1.amazonaws.com/images/member/623c20cfd6e5f001c0a829be.jpg";
 
-  // function CommentBox() {
-  //   const [comment, setComment] = useState("");
-  //   const [comments, setComments] = useState([]);
-  //   const [userComments, setUserComments] = useState({});
-  //   const [editIndex, setEditIndex] = useState(null);
-  //   const [selectedCommentIndex, setSelectedCommentIndex] = useState(null);
-
-  //   const commentBoxRef = useRef(null);
-
-  //   useEffect(() => {
-  //     // Event listener to handle click outside the comment area and cancel editing
-  //     const handleClickOutside = (event) => {
-  //       if (
-  //         commentBoxRef.current &&
-  //         !commentBoxRef.current.contains(event.target)
-  //       ) {
-  //         handleCancel();
-  //       }
-  //     };
-
-  //     document.addEventListener("mousedown", handleClickOutside);
-
-  //     return () => {
-  //       document.removeEventListener("mousedown", handleClickOutside);
-  //     };
-  //   }, []);
-
-  //   const handleChange = (event) => {
-  //     const { value } = event.target;
-  //     setComment(value.substring(0, 300));
-  //   };
-
-  //   const handleSubmit = () => {
-  //     if (comment.trim()) {
-  //       if (editIndex !== null) {
-  //         setComments((prevComments) => {
-  //           const updatedComments = [...prevComments];
-  //           updatedComments[editIndex] = comment.trim();
-  //           return updatedComments;
-  //         });
-  //         setUserComments((prevUserComments) => {
-  //           const updatedUserComments = { ...prevUserComments };
-  //           updatedUserComments[editIndex] = {
-  //             text: comment.trim(),
-  //             date: new Date(),
-  //           };
-  //           return updatedUserComments;
-  //         });
-  //         setEditIndex(null);
-  //       } else {
-  //         setComments((prevComments) => [...prevComments, comment.trim()]);
-  //         setUserComments((prevUserComments) => ({
-  //           ...prevUserComments,
-  //           [comments.length]: { text: comment.trim(), date: new Date() },
-  //         }));
-  //       }
-  //       setComment("");
-  //     }
-  //   };
-
-  //   const handleEdit = (index) => {
-  //     setComment(comments[index]);
-  //     setEditIndex(index);
-  //     setSelectedCommentIndex(index);
-  //   };
-
-  //   const handleDelete = (index) => {
-  //     setComments((prevComments) => prevComments.filter((_, i) => i !== index));
-  //     setUserComments((prevUserComments) => {
-  //       const updatedUserComments = { ...prevUserComments };
-  //       delete updatedUserComments[index];
-  //       return updatedUserComments;
-  //     });
-  //     setSelectedCommentIndex(null);
-  //   };
-
-  //   const handleCancel = () => {
-  //     setComment("");
-  //     setEditIndex(null);
-  //     setSelectedCommentIndex(null);
-  //   };
-
-  //   const commentCount = comments.length;
-
-  //   return (
-  //     <>
-  //       {data}
-  //       <div className="bg-white shadow p-4 w-full ">
-  //         <div className=" relative border border-black">
-  //           <textarea
-  //             className="w-full focus:outline-0"
-  //             rows="7"
-  //             placeholder="Silahkan tinggalkan komentar terkait event ini."
-  //             value={comment}
-  //             onChange={handleChange}
-  //           ></textarea>
-  //           <button
-  //             className="bg-slate-200 hover:bg-slate-400 opacity-50 text-black text-center text-sm font-sans  py-0 px-0 absolute bottom-2  right-6"
-  //             onClick={handleSubmit}
-  //             disabled={comment.trim().length === 0}
-  //           >
-  //             Submit Komentar
-  //           </button>
-  //           <div className="absolute bottom-2 right-36">
-  //             <p className="text-sm text-gray-500">
-  //               {comment.length}/300 characters
-  //             </p>
-  //           </div>
-  //         </div>
-  //         <div className="border rounded-lg border-hidden p-2 mt-4">
-  //           {comments.map((c, index) => (
-  //             <div key={index} className="flex items-start flex-col mb-2">
-  //               <div className="bg-gray-300 h-10 w-10 rounded-full mb-2"></div>
-  //               <div className="flex justify-between w-full">
-  //                 <div>
-  //                   <p className="font-bold">User {index + 1}</p>
-  //                   <p className="text-sm">{c}</p>
-  //                   <p className="text-xs text-gray-500">
-  //                     {getTimeAgo(new Date())}
-  //                   </p>
-  //                 </div>
-  //                 {userComments[index] && (
-  //                   <div className="relative">
-  //                     <button
-  //                       className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-1 px-2 rounded"
-  //                       onClick={() => handleEdit(index)}
-  //                     >
-  //                       Edit
-  //                     </button>
-  //                     {selectedCommentIndex === index && (
-  //                       <div className="absolute top-0 right-8 flex flex-col space-y-2 p-2 bg-white border border-gray-300 rounded-lg shadow">
-  //                         <button
-  //                           className="text-red-500 hover:text-red-600 font-bold"
-  //                           onClick={() => handleDelete(index)}
-  //                         >
-  //                           Delete
-  //                         </button>
-  //                         <button
-  //                           className="text-yellow-500 hover:text-yellow-600 font-bold"
-  //                           onClick={() => handleReport(index)}
-  //                         >
-  //                           Report
-  //                         </button>
-  //                       </div>
-  //                     )}
-  //                   </div>
-  //                 )}
-  //               </div>
-  //             </div>
-  //           ))}
-  //         </div>
-  //       </div>
-  //     </>
-  //   );
-  // }
   function CommentBox(dataId) {
     const [inputComment, setInputComment] = useState("");
     function HandleChange(e) {
@@ -222,8 +67,6 @@ function DetailEvent() {
           ></textarea>
           <button
             className="bg-slate-200 hover:bg-slate-400 opacity-50 text-black text-center text-sm font-sans  py-0 px-0 absolute bottom-2  right-6"
-            // onClick={handleSubmit}
-            // disabled={comment.trim().length === 0}
             type="submit"
           >
             Submit Komentar
@@ -237,7 +80,60 @@ function DetailEvent() {
       </>
     );
   }
-
+  function DisplayComment() {
+    function HitungUmur(birthYear) {
+      let birthDate = new Date(birthYear)
+      let difference = Date.now() - birthDate.getTime();
+      let ageDate = new Date(difference);
+      let result = Math.abs(ageDate.getUTCFullYear() - birthYear);
+      console.log(result," <<<");
+      if(birthYear <=10) {
+        return "20 -an"
+      }
+      if(result <= 29) {
+        return "20 -an"
+      }else if (result <= 39) {
+        return "30 -an"
+      }else if (result <= 49) {
+        return "40 -an"
+      }else {
+        return  "50 -an"
+      }
+    }
+  
+    return (
+      <>
+        {dataComment?.map((el) => (
+          console.log(el),
+          <div className="flex flex-row border-b-2 space-y-4" key={el.nameReviewer}>
+            <div className="flex flex-col w-full">
+              <div className="flex items-start flex-row mb-2 w-full space-x-5">
+                <div className="bg-gray-300 h-19 w-16 rounded-full mb-2">
+                  {/* Ganti imageUrl dengan el.imageUrl */}
+                  <ImageComponent imageUrl={el.imageUrl} />
+                </div>
+                <div className="flex flex-col w-full">
+                  <div className="flex flex-col">
+                    <p className="font-md">{el.nameReviewer}</p>
+                    <p className="text-xs text-gray-500">
+                      {HitungUmur(el.birthYear)} | {el.skinType} | {el.skinColor}
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="space-y-5">
+                <p className="text-sm">{el.descReviewer}</p>
+              </div>
+            </div>
+            <p className="flex justify-end text-xs text-gray-500 w-40">
+              {getTimeAgo(new Date())}
+            </p>
+          </div>
+        ))}
+      </>
+    );
+  }
+  
   function Display() {
     return (
       <>
@@ -248,14 +144,11 @@ function DetailEvent() {
             </Link>
           </div>
           <div className="self-center flex justify-center font-bold">
-          <h1>EVENT</h1>
+            <h1>EVENT</h1>
           </div>
-          <div className="self-center">
-          </div>
+          <div className="self-center"></div>
         </div>
-        <div>
-          
-        </div>
+        <div></div>
         {dataDetail?.map((el) => {
           const lastDate = el.startDate;
           const firstDate = el.endDate;
@@ -356,36 +249,8 @@ function DetailEvent() {
               </div>
               <div>
                 <div className="border rounded-lg border-hidden p-2 mt-4">
-                  {dataComment?.map((el) => (
-                    <div className=" flex flex-row border-b-2 space-y-4">
-                      <div className="flex flex-col w-full">
-                        <div
-                          key={el.nameReviewer}
-                          className="flex items-start flex-row mb-2 w-full space-x-5"
-                        >
-                          <div className="bg-gray-300 h-19 w-16 rounded-full mb-2">
-                            <ImageComponent imageUrl={imageUrl} />
-                          </div>
-                          <div className="flex flex-col w-full">
-                            <div className="flex flex-col">
-                              <p className="font-md">{el.nameReviewer}</p>
-                              <p className="text-xs text-gray-500">
-                                {el.birthyear}|{el.skinType}|{el.skinColor}
-                              </p>
-                            </div>
-                          </div>
-                        </div>
-                        <div className=" space-y-5">
-                          <p className="text-sm">{el.descReviewer}</p>
-                        </div>
-                      </div>
-                      <p className="flex justify-end text-xs text-gray-500 w-40">
-                        {getTimeAgo(new Date())}
-                      </p>
-                    </div>
-                  ))}
-        <div className="relative mb-10"></div>
-
+                  <DisplayComment />
+                  <div className="relative mb-10"></div>
                 </div>
               </div>
             </div>
@@ -396,10 +261,9 @@ function DetailEvent() {
   }
   return (
     <>
-    <Display />
-    
+      <Display />
     </>
-    )
+  );
 }
 
 export default DetailEvent;
