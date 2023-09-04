9921f25c135245034956139e84a046254a9adf22
gustavsr1994 <gustavr1994@gmail.com>
2023-09-04 14:01:24 +0700

    update view

--- js/main.js
+++ js/main.js
@@ -1,63 +1,58 @@
 // 메인 슬릭 슬라이더
 $(function () {
-    $('#main_slider').slick({
+    $(".slider").slick({
         dots: true,
         infinite: true,
         autoplaySpeed: 4000,
         fade: true,
-        cssEase: 'linear',
+        cssEase: "linear",
         autoplay: true,
         speed: 500,
-        prevArrow: `<button type='button' class='slick-prev'><i class="fas fa-chevron-left"></i></button>`,
-        nextArrow: `<button type='button' class='slick-next'><i class="fas fa-chevron-right"></i></button>`,
-        // swipeToSlide:true
+        prevArrow: `<button type='button' className='slick-prev'><i className="fas fa-chevron-left"></i></button>`,
+        nextArrow: `<button type='button' className='slick-next'><i className="fas fa-chevron-right"></i></button>`,
+        swipeToSlide: true,
     });
-})
+});
 
 // scroll event
-window.addEventListener('scroll', scrollWork, {
-    passive: true
-})
+window.addEventListener("scroll", scrollWork, {
+    passive: true,
+});
 
-const html = document.querySelector('html');
-const cosmeticLis = document.querySelectorAll('#cosmetic_box li')
-const calendarTexts = document.querySelectorAll('.calendar_text')
-const stepDivs = document.querySelectorAll('.step')
+const html = document.querySelector("html");
+const cosmeticLis = document.querySelectorAll("#cosmetic_box li");
+const calendarTexts = document.querySelectorAll(".calendar_text");
+const stepDivs = document.querySelectorAll(".step");
 
 function scrollWork() {
-
     if (html.scrollTop > 1700 && html.scrollTop < 1800) {
-
         for (i = 0; i < calendarTexts.length; i++) {
-            calendarTexts[i].classList.add('ani')
+            calendarTexts[i].classList.add("ani");
         }
     } else if (html.scrollTop > 1000 && html.scrollTop < 1400) {
-
         for (i = 0; i < cosmeticLis.length; i++) {
-            cosmeticLis[i].classList.add('ani')
+            cosmeticLis[i].classList.add("ani");
         }
     } else if (html.scrollTop > 1800 && html.scrollTop < 2000) {
-
         for (i = 0; i < stepDivs.length; i++) {
-            stepDivs[i].classList.add('step_ani')
+            stepDivs[i].classList.add("step_ani");
         }
     }
 }
 
 // navigation
-const hamburgerBtn = document.querySelector('.hamburger')
-const navUl = document.querySelector('.nav_wrap ul')
+const hamburgerBtn = document.querySelector(".hamburger");
+const navUl = document.querySelector(".nav_wrap ul");
 
-navUl.classList.remove('on')
+navUl.classList.remove("on");
 
-hamburgerBtn.addEventListener('click', () => {
-
+hamburgerBtn.addEventListener("click", () => {
     // 클릭하면 바뀌고 한번 더 클릭하면 원상태로
-    if (!hamburgerBtn.classList.contains('toggle')) {
-        hamburgerBtn.classList.add('toggle');
-        navUl.classList.add('on')
+    if (!hamburgerBtn.classList.contains("toggle")) {
+        hamburgerBtn.classList.add("toggle");
+        navUl.classList.add("on");
     } else {
-        hamburgerBtn.classList.remove('toggle')
-        navUl.classList.remove('on')
+        hamburgerBtn.classList.remove("toggle");
+        navUl.classList.remove("on");
     }
 });
