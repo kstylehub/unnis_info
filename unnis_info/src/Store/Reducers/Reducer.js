1a0a5c592b5f84998e2ffd41554fb46465cef1d5
Dea Noveriyanti <95268820+Deoyski@users.noreply.github.com>
2024-06-27 09:57:18 +0700

    Update Reducer

--- unnis_info/src/Store/Reducers/Reducer.js
+++ unnis_info/src/Store/Reducers/Reducer.js
@@ -59,7 +59,7 @@ export function ReducerTopProduct(state = listTopProduct, action) {
 }
 
 const detailProduct = {
-  dataProduct: [],
+  dataDetailProduct: [],
   error: null,
   loading: true,
 };
@@ -71,7 +71,7 @@ export function ReducerDetailProduct(state = detailProduct, action) {
     case PRODUCT.GET_DETAIL_PRODUCT_FAILED:
       return { ...state, error: action.payload, loading: false };
     case PRODUCT.GET_DETAIL_PRODUCT_SUCCESS:
-      return { ...state, dataProduct: action.payload, loading: false };
+      return { ...state, dataDetailProduct: action.payload, loading: false };
     default:
       return state;
   }
@@ -99,25 +99,28 @@ export function ReducerAllProduct(state = allProduct, action) {
       return state;
   }
 }
-  const allProductWithPagination = {
-    dataProductWithPagination: [],
-    error: null,
-    loading: true,
-  };
-  
-  export function ReducerProductWithPagination(state = allProductWithPagination, action) {
-    switch (action.type) {
-      case PRODUCT.GET_PAGINATION_PRODUCT_START:
-        return { ...state, loading: true };
-      case PRODUCT.GET_PAGINATION_PRODUCT_FAILED:
-        return { ...state, loading: false, error: action.payload };
-      case PRODUCT.GET_PAGINATION_PRODUCT_SUCCESS:
-        return {
-          ...state,
-          loading: false,
-          dataProductWithPagination: action.payload,
-        };
-      default:
-        return state;
-    }
+const allProductWithPagination = {
+  dataProductWithPagination: [],
+  error: null,
+  loading: true,
+};
+
+export function ReducerProductWithPagination(
+  state = allProductWithPagination,
+  action
+) {
+  switch (action.type) {
+    case PRODUCT.GET_PAGINATION_PRODUCT_START:
+      return { ...state, loading: true };
+    case PRODUCT.GET_PAGINATION_PRODUCT_FAILED:
+      return { ...state, loading: false, error: action.payload };
+    case PRODUCT.GET_PAGINATION_PRODUCT_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        dataProductWithPagination: action.payload,
+      };
+    default:
+      return state;
   }
+}
--- unnis_info/src/Store/Reducers/ReducerVideo.js
+++ unnis_info/src/Store/Reducers/ReducerVideo.js
@@ -1,25 +1,99 @@
-import ACTION_TYPES from "../Constans/ActionTypes"
+import ACTION_TYPES from "../Constans/ActionTypes";
 
-const { VIDEO } = ACTION_TYPES
+const { VIDEO } = ACTION_TYPES;
 
-const defaultValue ={
-    idVideo:[],
-    error:null,
-    loading:true,
+const defaultValue = {
+  idVideo: [],
+  error: null,
+  loading: true,
+};
+export function ReducerVideoByIdMemberYoutube(state = defaultValue, action) {
+  switch (action.type) {
+    case VIDEO.GET_DATA_VIDEO_START:
+      return { ...state, loading: true };
+    case VIDEO.GET_DATA_VIDEO_FAILED:
+      return { ...state, error: action.payload, loading: false };
+    case VIDEO.GET_DATA_VIDEO_SUCCESS:
+      return { ...state, idVideo: action.payload, loading: false };
+
+    default:
+      return state;
+  }
 }
-export function ReducerVideoByIdMember(
-    state=defaultValue,
-    action,
-){
-    switch (action.type) {
-        case VIDEO.GET_DATA_VIDEO_START:
-            return {...state, loading: true }
-        case VIDEO.GET_DATA_VIDEO_FAILED:
-            return {...state, error: action.payload, loading:false }
-        case VIDEO.GET_DATA_VIDEO_SUCCESS:
-            return {...state, idVideo: action.payload.data, loading:false }
-    
-        default:
-            return state;
-    }
+
+const videoValue = {
+  videoInstagram: [],
+  error: null,
+  loading: true,
+};
+export function ReducerVideoByIdMemberInstagram(state = videoValue, action) {
+  switch (action.type) {
+    case VIDEO.GET_DATA_VIDEO_INSTAGRAM_START:
+      return { ...state, loading: true };
+    case VIDEO.GET_DATA_VIDEO_INSTAGRAM_FAILED:
+      return { ...state, error: action.payload, loading: false };
+    case VIDEO.GET_DATA_VIDEO_INSTAGRAM_SUCCESS:
+      return { ...state, videoInstagram: action.payload, loading: false };
+
+    default:
+      return state;
+  }
+}
+
+const videoUnnisValue = {
+  videoUnnis: [],
+  error: null,
+  loading: true,
+};
+export function ReducerVideoByIdMemberUnnis(state = videoUnnisValue, action) {
+  switch (action.type) {
+    case VIDEO.GET_DATA_VIDEO_UNNIS_START:
+      return { ...state, loading: true };
+    case VIDEO.GET_DATA_VIDEO_UNNIS_FAILED:
+      return { ...state, error: action.payload, loading: false };
+    case VIDEO.GET_DATA_VIDEO_UNNIS_SUCCESS:
+      return { ...state, videoUnnis: action.payload.data, loading: false };
+
+    default:
+      return state;
+  }
+}
+
+const valueProductRecommen = {
+  productrecommen: [],
+  error: null,
+  loading: true,
+};
+export function ReducerProductRecommendationVideo(
+  state = valueProductRecommen,
+  action
+) {
+  switch (action.type) {
+    case VIDEO.GET_DATA_VIDEO_PRODUCT_RECOMMENDATION_START:
+      return { ...state, loading: true };
+    case VIDEO.GET_DATA_VIDEO_PRODUCT_RECOMMENDATION_FAILED:
+      return { ...state, error: action.payload, loading: false };
+    case VIDEO.GET_DATA_VIDEO_PRODUCT_RECOMMENDATION_SUCCESS:
+      return { ...state, productrecommen: action.payload.data, loading: false };
+    default:
+      return state;
+  }
+}
+
+const valueProduct = {
+  productVideo: [],
+  error: null,
+  loading: true,
+};
+export function ReducerProductVideo(state = valueProduct, action) {
+  switch (action.type) {
+    case VIDEO.GET_DATA_VIDEO_PRODUCT_START:
+      return { ...state, loading: true };
+    case VIDEO.GET_DATA_VIDEO_PRODUCT_FAILED:
+      return { ...state, error: action.payload, loading: false };
+    case VIDEO.GET_DATA_VIDEO_PRODUCT_SUCCESS:
+      return { ...state, productVideo: action.payload, loading: false };
+    default:
+      return state;
+  }
 }
