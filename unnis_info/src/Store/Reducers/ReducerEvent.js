4c23e2319a059f0ff5acf76577338e8a761ace65
Dea Noveriyanti <95268820+Deoyski@users.noreply.github.com>
2024-07-04 08:53:47 +0700

    Update reducer

--- unnis_info/src/Store/Reducers/ReducerEvent.js
+++ unnis_info/src/Store/Reducers/ReducerEvent.js
@@ -23,3 +23,47 @@ export function ReducerEventData(
             return state;
     }
 }
+
+const defaultValue2 ={
+    eventById:[],
+    error:null,
+    loading:true,
+}
+export function ReducerEventById(
+    state=defaultValue2,
+    action,
+){
+    switch (action.type) {
+        case EVENT.GET_EVENT_ID_START:
+            return {...state, loading: true }
+        case EVENT.GET_EVENT_ID_FAILED:
+            return {...state, error: action.payload, loading:false }
+        case EVENT.GET_EVENT_ID_SUCCESS:
+            return {...state, eventById: action.payload, loading:false }
+    
+        default:
+            return state;
+    }
+}
+
+const defaultValue3 ={
+    eventReviewById:[],
+    error:null,
+    loading:true,
+}
+export function ReducerReviewEventById(
+    state=defaultValue3,
+    action,
+){
+    switch (action.type) {
+        case EVENT.GET_EVENT_REVIEW_ID_START:
+            return {...state, loading: true }
+        case EVENT.GET_EVENT_REVIEW_ID_FAILED:
+            return {...state, error: action.payload, loading:false }
+        case EVENT.GET_EVENT_REVIEW_ID_SUCCESS:
+            return {...state, eventReviewById: action.payload, loading:false }
+    
+        default:
+            return state;
+    }
+}
--- unnis_info/src/Store/Reducers/ReducerFeed.js
+++ unnis_info/src/Store/Reducers/ReducerFeed.js
@@ -20,3 +20,41 @@ export function ReducerFeed(state= defaultValue, action){
             return state
     }
 }
+
+const defaultValue1 = {
+    dataFeedById: [],
+    error: null,
+    loading: true
+}
+
+export function ReducerAllFeedById(state= defaultValue1, action){
+    switch (action.type) {
+        case FEED.GET_ALL_FEED_BY_ID_START:
+            return{...state, loading: true}
+        case FEED.GET_ALL_FEED_BY_ID_FAILED: 
+            return{...state, error: action.payload, loading: false}
+        case FEED.GET_ALL_FEED_BY_ID_SUCCESS:
+            return{...state, dataFeedById: action.payload, loading: false}
+        default:
+            return state
+    }
+}
+
+const defaultValue2 = {
+    dataReviewFeed: [],
+    error: null,
+    loading: true
+}
+
+export function ReducerReviewFeed(state= defaultValue2, action){
+    switch (action.type) {
+        case FEED.GET_REVIEW_FEED_BY_ID_START:
+            return{...state, loading: true}
+        case FEED.GET_REVIEW_FEED_BY_ID_FAILED: 
+            return{...state, error: action.payload, loading: false}
+        case FEED.GET_REVIEW_FEED_BY_ID_SUCCESS:
+            return{...state, dataReviewFeed: action.payload, loading: false}
+        default:
+            return state
+    }
+}
